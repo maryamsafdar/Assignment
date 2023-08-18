f22ea64867b31efc7e67214768f110f723a5bfc3
Maryam Safdar <maryamsafdar453@gmail.com>
2023-08-18 21:27:26 +0500

    first commit

---
+++ task5.ts
@@ -0,0 +1,4 @@
+let famous_Quote: string = "A person who never made a mistake never tried anything new.";
+let author1: string = "Albert Einstein";
+let message :string = `${author1} once said , ${famous_Quote}`
+console.log(message)
